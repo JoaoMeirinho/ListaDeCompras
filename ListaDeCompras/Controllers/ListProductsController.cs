a6015ccb54aa9c811616a5bcb23d835e35c212f3
João Meirinho <jao46roberto@gmail.com>
2024-03-19 12:56:26 -0300

    create ListProducts controller and add index method

---
+++ ListaDeCompras/Controllers/ListProductsController.cs
@@ -0,0 +1,21 @@
+﻿using ListaDeCompras.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ListaDeCompras.Controllers
+{
+    public class ListProductsController : Controller
+    {
+        private readonly ListaDeComprasContext _context;
+
+        public ListProductsController(ListaDeComprasContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(_context.ListProducts.ToListAsync());
+        }
+    }
+}
